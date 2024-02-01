84fc118393a237484e23e0472834a7ebb9c840d8
oussama sabbar <sebbaroussama3@gmail.com>
2024-02-01 17:57:30 +0100

    mini

--- main.c
+++ main.c
@@ -87,12 +87,14 @@ void ft_output_execution(mini_t *mini, char **env, char *cmd)
     {
         mini->args = ft_split(cmd , ' ');
         mini->path = ft_getpath(mini->args[0], env);
-        dup2(mini->fd[0], STDIN_FILENO);
+        dup2(mini->input, STDIN_FILENO);
         dup2(STDOUT_FILENO, mini->fd[1]);
         close(mini->fd[1]);
+        close(mini->fd[0]);
         if (execve(mini->path, mini->args, env) == -1)
         {
             printf("command not found\n");
+            exit(1);
         }
     }
 }
@@ -105,10 +107,14 @@ void ft_input_execution(mini_t *mini, char **env, char *cmd)
     {
         mini->args = ft_split(cmd , ' ');
         mini->path = ft_getpath(mini->args[0], env);
-        dup2(mini->fd[0], STDIN_FILENO);
+        dup2(mini->fd[1], STDOUT_FILENO);
+        dup2(mini->input, STDIN_FILENO);
+        close(mini->fd[0]);
+        // close(mini->fd[1]);
         if (execve(mini->path, mini->args, env) == -1)
         {
             printf("command not found\n");
+            exit(1);
         }
     }
 }
@@ -123,34 +129,35 @@ void exec_first_cmd(mini_t *mini, char *cmd,char **env)
         mini->args = ft_split(cmd , ' ');
         mini->path = ft_getpath(mini->args[0], env);
         dup2(mini->fd[1], STDOUT_FILENO);
+        close(mini->fd[0]);
         if (execve(mini->path, mini->args, env) == -1)
         {
             printf("command not found\n");
+            exit(1);
         }
     }
 }
 void multiple_cmds(mini_t *mini, char **env)
 {
     char **piped_command = ft_split(mini->cmd, '|');
-    int i = 1;
     int j = 0;
     
     if (pipe(mini->fd) == -1)
         exit(1);
     exec_first_cmd(mini, piped_command[0], env);
+    close(mini->fd[1]);
+    int i = 1;
     while (piped_command[i])
     {
+        wait(NULL);
+        mini->input = mini->fd[0];
+        if (pipe(mini->fd) == -1)
+            exit(1);
         if (piped_command[i + 1] == NULL)
-        {
             ft_output_execution(mini, env, piped_command[i]);
-            break;
-        }
         else
             ft_input_execution(mini, env, piped_command[i]);
-        // close(mini->fd[1]);
-        wait(NULL);
-        // if (pipe(mini->fd) == -1)
-        //     exit(1);
+        close(mini->fd[1]);
         i++;
     }
 }
