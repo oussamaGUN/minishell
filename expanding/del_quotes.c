63fa5ea5febab19cb43fc053897369e686ea559a
Oussama Sabbar <ousabbar@e1r13p12.1337.ma>
2024-04-14 18:13:11 +0100

    mini

--- expanding/del_quotes.c
+++ expanding/del_quotes.c
@@ -11,6 +11,13 @@ char *expand(char *s, t_env *envp)
     }
     return NULL;
 }
+void ft_exit_value(t_multx *exp_vars, char *s)
+{
+    exp_vars->i++;
+    char *str = ft_itoa(exit_status >> 8);
+    exp_vars->res = ft_strjoin(exp_vars->res, str);
+    exp_vars->k = ft_strlen(exp_vars->res);
+}
 void ft_delemiter(t_multx *exp_vars, char *s)
 {
     exp_vars->res = malloc((ft_strlen(s) + 1) * 1);
@@ -22,6 +29,27 @@ void ft_delemiter(t_multx *exp_vars, char *s)
     }
     exp_vars->res[exp_vars->j] = '\0';
 }
+void ft_expanding_the_inside(t_multx *exp_vars, char *s, t_env *env)
+{
+    exp_vars->i++;
+    while (s[exp_vars->i] != '$' && s[exp_vars->i] != ' ' 
+        && s[exp_vars->i] != '\"' && ft_isalnum(s[exp_vars->i]))
+    {
+        exp_vars->inside_dquotes[exp_vars->j] = s[exp_vars->i];
+        exp_vars->i++;
+        exp_vars->j++;
+    }
+    exp_vars->inside_dquotes[exp_vars->j] = '\0';
+    exp_vars->exp = expand(exp_vars->inside_dquotes, env);
+    if (exp_vars->exp)
+    {
+        exp_vars->res = ft_strjoin(exp_vars->res, exp_vars->exp);
+        exp_vars->k = ft_strlen(exp_vars->res);
+    }
+    else
+        exp_vars->res = ft_strjoin(exp_vars->res, "");
+    exp_vars->i--;
+}
 void ft_double_quotes_expand(t_multx *exp_vars, char *s, t_env *env)
 {
     exp_vars->i++;
@@ -29,43 +57,16 @@ void ft_double_quotes_expand(t_multx *exp_vars, char *s, t_env *env)
     {
         exp_vars->j = 0;
         if (s[exp_vars->i] == '$' && s[exp_vars->i + 1] == '?')
-        {
-            exp_vars->i++;
-            char *str = ft_itoa(exit_status >> 8);
-            exp_vars->res = ft_strjoin(exp_vars->res, str);
-            exp_vars->k = ft_strlen(exp_vars->res);
-            
-        }
+            ft_exit_value(exp_vars, s);
         else if (s[exp_vars->i] == '$' && s[exp_vars->i + 1] != ' ' && s[exp_vars->i + 1] != '\'' 
             && s[exp_vars->i + 1] != '\"' && s[exp_vars->i + 1]&& !ft_isdigit(s[exp_vars->i + 1]))
-        {
-            exp_vars->i++;
-            while (s[exp_vars->i] != '$' && s[exp_vars->i] != ' ' 
-                && s[exp_vars->i] != '\"' && ft_isalnum(s[exp_vars->i]))
-            {
-                exp_vars->inside_dquotes[exp_vars->j] = s[exp_vars->i];
-                exp_vars->i++;
-                exp_vars->j++;
-            }
-            exp_vars->inside_dquotes[exp_vars->j] = '\0';
-            exp_vars->exp = expand(exp_vars->inside_dquotes, env);
-            if (exp_vars->exp)
-            {
-                exp_vars->res = ft_strjoin(exp_vars->res, exp_vars->exp);
-                exp_vars->k = ft_strlen(exp_vars->res);
-            }
-            else
-                exp_vars->res = ft_strjoin(exp_vars->res, "");
-            exp_vars->i--;
-        }
+            ft_expanding_the_inside(exp_vars, s, env);
         else
         {
             if (s[exp_vars->i] == '$' && ft_isdigit(s[exp_vars->i + 1]))
                 exp_vars->i++;
             else
-            {
                 exp_vars->res = ft_strjoin(exp_vars->res, &s[exp_vars->i]);
-            }
         }
         if (exp_vars->res[exp_vars->k])
             exp_vars->k++;
@@ -74,7 +75,6 @@ void ft_double_quotes_expand(t_multx *exp_vars, char *s, t_env *env)
         exp_vars->res[exp_vars->k] = '\0';
     }
     exp_vars->i++;
-
 }
 void ft_single_quotes(t_multx *exp_vars, char *s)
 {
@@ -91,16 +91,7 @@ void ft_single_quotes(t_multx *exp_vars, char *s)
     exp_vars->i++;
     exp_vars->res[exp_vars->k] = '\0';
 }
-void ft_exit_value(t_multx *exp_vars, char *s)
-{
-    exp_vars->i++;
-    while (s[exp_vars->i] != '$' && s[exp_vars->i] != ' ' 
-        && s[exp_vars->i] != '\"' && s[exp_vars->i] != '\'' && ft_isalnum(s[exp_vars->i]))
-        exp_vars->i++;
-    char *str = ft_itoa(exit_status >> 8);
-    exp_vars->res = ft_strjoin(exp_vars->res, str);
-    exp_vars->k = ft_strlen(exp_vars->res);
-}
+
 void ft_expand_normalin_multi(t_multx *exp_vars, char *s, t_env *env)
 {
     exp_vars->i++;
@@ -142,43 +133,16 @@ void ft_expand_double_in_multiple(t_multx *exp_vars, char *s, t_env *env)
     {
         exp_vars->j = 0;
         if (s[exp_vars->i] == '$' && s[exp_vars->i + 1] == '?')
-        {
-            exp_vars->i++;
-            char *str = ft_itoa(exit_status >> 8);
-            exp_vars->res = ft_strjoin(exp_vars->res, str);
-            exp_vars->k = ft_strlen(exp_vars->res);
-            
-        }
+            ft_exit_value(exp_vars, s);
         else if (s[exp_vars->i] == '$' && s[exp_vars->i + 1] != ' ' && s[exp_vars->i + 1] != '\'' 
             && s[exp_vars->i + 1] != '\"' && s[exp_vars->i + 1]&& !ft_isdigit(s[exp_vars->i + 1]))
-        {
-            exp_vars->i++;
-            while (s[exp_vars->i] != '$' && s[exp_vars->i] != ' ' 
-                && s[exp_vars->i] != '\"' && ft_isalnum(s[exp_vars->i]))
-            {
-                exp_vars->inside_dquotes[exp_vars->j] = s[exp_vars->i];
-                exp_vars->i++;
-                exp_vars->j++;
-            }
-            exp_vars->inside_dquotes[exp_vars->j] = '\0';
-            exp_vars->exp = expand(exp_vars->inside_dquotes, env);
-            if (exp_vars->exp)
-            {
-                exp_vars->res = ft_strjoin(exp_vars->res, exp_vars->exp);
-                exp_vars->k = ft_strlen(exp_vars->res);
-            }
-            else
-                exp_vars->res = ft_strjoin(exp_vars->res, "");
-            exp_vars->i--;
-        }
+            ft_expanding_the_inside(exp_vars, s, env);
         else
         {
             if (s[exp_vars->i] == '$' && ft_isdigit(s[exp_vars->i + 1]))
                 exp_vars->i++;
             else
-            {
                 exp_vars->res = ft_strjoin(exp_vars->res, &s[exp_vars->i]);
-            }
         }
         if (exp_vars->res[exp_vars->k])
             exp_vars->k++;
