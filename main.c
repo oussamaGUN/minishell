3d42dbe1c63532171e5bb15eb607802bb8ab0f15
BL4KCY <mostafafarssiui@gmail.com>
2024-04-16 18:56:39 +0100

    edit main to be compatible with garbage collector

--- main.c
+++ main.c
@@ -6,7 +6,6 @@ void free_token(t_token *token)
 	while (token)
 	{
 		free(token->content);
-		free(token);
 		if (token->arr)
 		{
 			i = 0;
@@ -14,10 +13,11 @@ void free_token(t_token *token)
 				free(token->arr[i++]);
 			free(token->arr);
 		}
+		free(token);
 		token = token->next;
 	}
 }
-int	cmd_exe(t_token *token, t_env **env)
+int	cmd_exe(t_token *token, t_env *env)
 {
 	t_token	*new_token;
 	t_token	*cmd_list;
@@ -27,18 +27,18 @@ int	cmd_exe(t_token *token, t_env **env)
 	if (!cmd)
 		return (printf("exit\n"), 1);
 	add_history(cmd);
-	if (!tokenizer(cmd, &token, env))
+	if (!tokenizer(cmd, &token, &env))
 		return (0);
 	if (!ft_check_errors(token))
-		return (free_token(token), 0);
+		return (0);
 	new_token = expanding(token, env);
 	if (!new_token)
-		return (free_token(new_token), 0);
+		return (0);
 	cmd_list = ft_list(new_token, env);
 	if (!cmd_list)
-		return (free_token(cmd_list),0);
+		return (0);
 	if (!execution(cmd_list, env))
-		return (free_token(cmd_list), 0);
+		return (0);
 	return 0;
 }
 
@@ -49,10 +49,11 @@ int	main(int ac, char *av[], char *envp[])
 	env = envir(envp);
 	if (!env)
 		return (printf("exit\n"), EXIT_FAILURE);
-	while (!cmd_exe(NULL, &env))
-		;
+	env->mem = NULL;
+	while (!cmd_exe(NULL, env))
+		garbage_collector(&(env->mem));
 	env_clear(&env);
 	rl_clear_history();
-	// system("leaks minishell");
+	system("leaks minishell");
 	return (EXIT_SUCCESS);
 }
