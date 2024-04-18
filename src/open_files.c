cfd705c333d39cf568c12e64953b58955b4066e2
oussamaGUN <sebbaroussama3@gmail.com>
2024-04-18 15:47:04 +0100

    change the return value

--- src/open_files.c
+++ src/open_files.c
@@ -6,7 +6,7 @@
 /*   By: ousabbar <ousabbar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 11:06:36 by ousabbar          #+#    #+#             */
-/*   Updated: 2024/04/18 11:07:29 by ousabbar         ###   ########.fr       */
+/*   Updated: 2024/04/18 15:13:52 by ousabbar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,13 @@ t_token	*ft_openning_files(t_token *token, t_token *node)
 	}
 	else if (token->type == FILE_IN)
 	{
+		if (access(token->content, F_OK | R_OK) == -1)
+			exit_status = 1 << 8;
 		node->input_file = open(token->content, O_RDONLY);
 		if (node->input_file == -1)
 		{
 			printf("bash: %s: No such file or directory\n", token->content);
-			return (NULL);
+			return (token);
 		}
 	}
 	else if (token->type == FILE_APP)
