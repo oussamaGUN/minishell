f474597f1fab25236b51f96346fc9ade38d9bd1b
oussamaGUN <sebbaroussama3@gmail.com>
2024-04-19 21:30:19 +0100

    rm some vars and conditions

--- src/check_errors.c
+++ src/check_errors.c
@@ -6,7 +6,7 @@
 /*   By: ousabbar <ousabbar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 09:33:54 by ousabbar          #+#    #+#             */
-/*   Updated: 2024/04/18 10:23:37 by ousabbar         ###   ########.fr       */
+/*   Updated: 2024/04/19 21:20:20 by ousabbar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,11 @@
 
 t_token	*ft_check_errors(t_token *token)
 {
-	int	flag;
-
-	flag = syntax_error(token);
-	if (flag == 0)
+	if (!syntax_error(token))
 	{
 		ft_putendl_fd("syntax error", 2);
 		exit_status = 258 << 8;
 		return (NULL);
 	}
-	else if (flag == 2)
-		return (NULL);
 	return (token);
 }
