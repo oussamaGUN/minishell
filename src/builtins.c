2e94796a9baea6aac77f905527b6e713b56f50ca
BL4KCY <mostafafarssiui@gmail.com>
2024-04-19 10:05:21 +0100

    refacotr builtins

--- src/builtins.c
+++ src/builtins.c
@@ -15,27 +15,37 @@ int pwd(t_token *lst, t_env *env)
 	char *path;
 	path = getcwd(NULL, 0);
 	if (!path)
-	{
-		printf("mini: No such file or directory\n");
-		return 0;
-	}
+		return (perror("mini"), 0);
 	else
 	{
 		lst->path = ft_malloc(0, &(env->mem), ft_strdup(path));
 		printf("%s\n", lst->path);
 	}
-	return 1;
+	return (1);
 }
 int cd(char **arr, t_env *env)
 {
 	if (!arr[1])
 	{
 		if (chdir(get_value(env, "HOME")) == -1)
-			return (perror("mini"), 0);
+			perror("mini");
 	}
-	else if (chdir(arr[1]) == -1)
-		return (perror("mini"), 0);
-	return 1;
+	else if (ft_strncmp(arr[1], "-", 1) == 0)
+	{
+		if (chdir(get_value(env, "OLDPWD")) == -1)
+			perror("mini");
+	}
+	else if (ft_strncmp(arr[1], "~", 1) == 0)
+	{
+		if (chdir(get_value(env, "HOME")) == -1)
+			perror("mini");
+	}
+	else
+	{
+		if (chdir(arr[1]) == -1)
+			perror("mini");
+	}
+	return (0);
 }
 t_env *ft_update_pwd_env(t_env *env)
 {
@@ -69,15 +79,14 @@ t_env *ft_update_oldpwd_env(t_env *env)
 	}
 	return env;
 }
-int env_print(t_env *env)
+int	print_env(t_env *env)
 {
-	t_env *itter = env;
-	while (itter)
+	while (env)
 	{
-		printf("%s=%s\n", itter->key, itter->value);
-		itter = itter->next;
+		printf("%s=%s\n", env->key, env->value);
+		env = env->next;
 	}
-	return 1;
+	return (1);
 }
 int ft_dashn(char *s)
 {
@@ -123,17 +132,26 @@ int echo(t_token *lst)
 }
 int builtins(t_token *lst, t_env *env)
 {
+	if (!(lst->arr[0]))
+		return (1);
 	if (ft_strncmp(lst->arr[0], "cd", 3) == 0 || ft_strncmp(lst->arr[0], "CD", 3) == 0)
+	{
 		if (!cd(lst->arr, env))
-			return 0;
+			return (0);
+	}
 	else if (ft_strncmp(lst->arr[0], "pwd", 3) == 0 || ft_strncmp(lst->arr[0], "PWD", 3) == 0)
+	{
 		if (!pwd(lst, env))
-			return 0;
+			return (0);
+	}
 	else if (ft_strncmp(lst->arr[0], "env", 4) == 0)
-		if (!env_print(env))
-			return 0;
+	{
+		if (!print_env(env))
+			return (0);
+	}
 	else if (ft_strncmp(lst->arr[0], "echo", 5) == 0 || ft_strncmp(lst->arr[0], "echo", 5) == 0)
 		if (!echo(lst))
-			return 0;
-	return 1;
+			return (0);
+	return (1);
 }
+
