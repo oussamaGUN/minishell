a2ef0928b74f829319845efd636440ec927d34b1
mostafafarssiui@gmail.com <BL4KCY>
2024-04-22 15:08:25 +0100

    change global variable to extern

--- includes/main.h
+++ includes/main.h
@@ -20,9 +20,9 @@
 # include "history.h"
 # include "readline.h"
 
+extern int	exit_status;
 
 // tokenize
-int exit_status;
 typedef struct s_token
 {
 	pid_t pid;
@@ -57,8 +57,8 @@ typedef struct s_env
 
 typedef struct s_tokenizer
 {
-	size_t		words_count;
-	size_t		word_len;
+	int			words_count;
+	int			word_len;
 	char		**arr;
 	int 		flag;
 	int			i;
@@ -100,7 +100,7 @@ typedef enum e_type {
 
 
 
-// list 
+// list
 t_token	*add_token(void *content, t_free **alloc);
 void	ft_lstadd(t_token **lst, t_token *new);
 int	lstsize(t_token *lst);
@@ -110,63 +110,63 @@ int	lstsize(t_token *lst);
 
 
 
-void	clear(t_token **lst, void (*del)(void *));
-void del(void *s);
+// void	clear(t_token **lst, void (*del)(void *));
+void	del(void *s);
 
 // void		ft_quit(int sig);
 // t_list		*ft_lst_creat_env(mini_t *mini, char **env);
 
 
 
-int tokenizer(char *str, t_token **token, t_env **env);
+int		tokenizer(char *str, t_token **token, t_env **env);
 char	**ft_ownsplit(char const *s, char c, t_tokenizer *vars);
-t_token *ft_check_errors(t_token *token);
-int syntax_error(t_token *token);
-t_token *ft_list(t_token *token, t_env *env);
-t_token *expanding(t_token *token, t_env *env);
+t_token	*ft_check_errors(t_token *token);
+int		syntax_error(t_token *token);
+t_token	*ft_list(t_token *token, t_env *env);
+t_token	*expanding(t_token *token, t_env *env);
 // int here_doc(t_token *token);
-char *expand(char *s, t_env *envp);
-int	pipe_error(t_token *s);
-int	red_output_error(t_token *s);
-int	red_input_error(t_token *s);
-int	red_append_error(t_token *s);
+char 	*expand(char *s, t_env *envp);
+int		pipe_error(t_token *s);
+int		red_output_error(t_token *s);
+int		red_input_error(t_token *s);
+int		red_append_error(t_token *s);
 void	ft_expanding_the_inside(t_multx *exp_vars, char *s, t_env *env);
 void	ft_double_quotes_expand(t_multx *exp_vars, char *s, t_env *env);
 void	ft_single_quotes(t_multx *exp_vars, char *s, t_env *env);
 void	ft_expand_normalin_multi(t_multx *exp_vars, char *s, t_env *env);
 void	ft_expand_single_in_multiple(t_multx *exp_vars, char *s, t_env *env);
-void	ft_exit_value(t_multx *exp_vars, char *s, t_env *env);
+void	ft_exit_value(t_multx *exp_vars, t_env *env);
 void	ft_delemiter(t_multx *exp_vars, char *s, t_free **mem);
 char	*expand(char *s, t_env *envp);
 t_token	*init(t_token *node, t_multx *vars, t_token *token, t_free **mem);
 t_token	*norm2(t_token *token, t_env *env, t_token *node, t_multx *vars);
 t_token	*ft_openning_files(t_token *token, t_token *node);
-int	ft_words(t_token *token);
+int		ft_words(t_token *token);
 t_token	*here_doc_implement(t_token *token, t_token *node, t_env *env);
 void	here_doc_expand_norm(t_multx *vars, char *s, t_env *env);
 void	here_doc_expand_norm_two(t_multx *vars, char *s, t_env *env);
 char	*minisplit(char const *s, t_tokenizer *vars);
-int	ft_count_quotes(char const *s, t_tokenizer *vars);
-int	ft_word(char *s, char c, t_tokenizer *vars);
+int		ft_count_quotes(char const *s, t_tokenizer *vars);
+int		ft_word(char *s, char c, t_tokenizer *vars);
 void	first_ft_word(char *s, t_tokenizer *vars);
-int	ft_len(char const *s, char c);
+int		ft_len(char const *s, char c);
 // execution
 char	*ft_getpath(char *cmd, char **env);
-int exec(t_token *lst, t_env *env);
-int execution(t_token *lst, t_env *env);
+int		exec(t_token *lst, t_env *env);
+int		execution(t_token *lst, t_env *env);
 void	builtins(t_token *lst, t_env *env);
 int		single_builtins(t_token *lst, t_env *env);
-char *ft_env(t_env *env, char *s);
+// char	*ft_env(t_env *env, char *s);
 
 
 //env
-t_env *envir(char **envp);
+t_env	*envir(char **envp);
 void	list_for_env(t_env **lst, t_env *new);
-t_env *ft_update_pwd_env(t_env *env);
+t_env	*ft_update_pwd_env(t_env *env);
 void	env_clear(t_env **env);
 
 // garbage collector
-void	*ft_malloc(size_t	size, t_free **alloc, void *mem);
+char	*ft_malloc(size_t	size, t_free **alloc, void *mem);
 void	garbage_collector(t_free **alloc);
 void	add_front_mem(t_free **alloc, t_free *new);
 
