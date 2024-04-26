2bbd28ef1259f712711c91da8876080fb4dba8b3
BL4KCY <mostafafarssiui@gmail.com>
2024-04-26 18:06:11 +0100

    update exit_status of the check error

--- src/check_errors.c
+++ src/check_errors.c
@@ -6,7 +6,7 @@ t_token	*ft_check_errors(t_token *token)
 	if (!syntax_error(token))
 	{
 		ft_putendl_fd("syntax error", 2);
-		exit_status = 258 << 8;
+		exit_status = 258;
 		return (NULL);
 	}
 	return (token);
