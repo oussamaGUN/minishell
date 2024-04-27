94fd0bd85212a41cc83403a7fe0ddf01110a1e4a
BL4KCY <mostafafarssiui@gmail.com>
2024-04-27 19:45:38 +0100

    add new handler for signal funcs

--- src/signals.c
+++ src/signals.c
@@ -23,10 +23,20 @@ void sig_handler(int signum)
 		rl_redisplay();
 		exit_status = 1;
 	}
+}
+void sig_handler_child(int signum)
+{
+	if (signum == SIGINT)
+	{
+		printf("\n");
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		exit_status = 130;
+	}
 	if (signum == SIGQUIT)
 	{
 		printf("Quit: %d\n", SIGQUIT);
-		exit(131);
+		exit_status = 131;
 	}
 }
 
@@ -37,6 +47,6 @@ void signals_for_parent()
 }
 void signals_for_child()
 {
-	signal(SIGINT, SIG_DFL);
-	signal(SIGQUIT,sig_handler);
+	signal(SIGINT, sig_handler_child);
+	signal(SIGQUIT, sig_handler_child);
 }
