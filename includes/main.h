b201a8b43d380432efc1a1bc25e34343f86bdf48
BL4KCY <mostafafarssiui@gmail.com>
2024-05-02 08:59:35 +0100

    fix 42-norminette and check which os to include specific header for it

--- includes/main.h
+++ includes/main.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: melfersi <melfersi@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 08:24:56 by melfersi          #+#    #+#             */
+/*   Updated: 2024/05/02 08:46:34 by melfersi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MAIN_H
 # define MAIN_H
 
@@ -10,39 +22,45 @@
 # include <string.h>
 # include <sys/ioctl.h>
 # include <sys/wait.h>
-#include <sys/stat.h>
+# include <sys/stat.h>
 # include <termios.h>
 # include <sys/stat.h>
 # include <errno.h>
 # include <stdbool.h>
 
-
 # define READLINE_LIBRARY
-# include "history.h"
-# include "readline.h"
+# ifdef __APPLE__
+#  include "history.h"
+#  include "readline.h"
+# endif
 
-extern int	exit_status;
+# ifdef __linux__
+#  include <readline/readline.h>
+#  include <readline/history.h>
+# endif
+
+extern int	g_exit_status;
 
 // tokenize
 typedef struct s_token
 {
-	pid_t pid;
-	int	type;
-	char *content;
-	char **arr;
-	int output_file;
-	int input_file;
-	int fd[2];
-	int exit_status;
-	int status;
-	char *path;
-	int here_doc_check;
-	bool	execute;
-	struct s_token *next;
-	struct s_token *prev;
+	pid_t			pid;
+	int				type;
+	char			*content;
+	char			**arr;
+	int				output_file;
+	int				input_file;
+	int				fd[2];
+	int				exit_status;
+	int				status;
+	char			*path;
+	int				here_doc_check;
+	bool			execute;
+	struct s_token	*next;
+	struct s_token	*prev;
 }	t_token;
 
-typedef	struct s_free
+typedef struct s_free
 {
 	void			*mem;
 	struct s_free	*next;
@@ -55,41 +73,40 @@ typedef struct s_env
 	char			**envp;
 	struct s_free	*mem;
 	struct s_env	*next;
-}t_env;
-
+}	t_env;
 
 typedef struct s_tokenizer
 {
 	int			words_count;
 	int			word_len;
 	char		**arr;
-	int 		flag;
+	int			flag;
 	int			i;
 	int			j;
 	int			k;
 	char const	*s;
-	char 		*res;
-	int 		count1;
-	int 		count2;
+	char		*res;
+	int			count1;
+	int			count2;
 }				t_tokenizer;
-
 
 typedef struct s_multx
 {
-    char *normal;
-    char *inside_dquotes;
-    char *inside_squotes;
-    char *cpy;
-    char *exp;
-    char *res;
-	int i;
-    int k;
-    int j;
-	int count;
-	int quotes;
-}t_multx;
+	char	*normal;
+	char	*inside_dquotes;
+	char	*inside_squotes;
+	char	*cpy;
+	char	*exp;
+	char	*res;
+	int		i;
+	int		k;
+	int		j;
+	int		count;
+	int		quotes;
+}	t_multx;
 
-typedef enum e_type {
+typedef enum e_type
+{
 	CMD,
 	PIPE,
 	RED_OUTPUT,
@@ -101,26 +118,13 @@ typedef enum e_type {
 	FILE_IN,
 	DELIMITER,
 	NO_ACCESS,
-}t_type;
+}	t_type;
 
-
-
-// list
 t_token	*add_token(void *content, t_free **alloc);
 void	ft_lstadd(t_token **lst, t_token *new);
-int	lstsize(t_token *lst);
-// parsing
+int		lstsize(t_token *lst);
 
-
-
-
-
-// void	clear(t_token **lst, void (*del)(void *));
 void	del(void *s);
-
-// void		ft_quit(int sig);
-// t_list		*ft_lst_creat_env(mini_t *mini, char **env);
-
 
 void	exit_status_value(pid_t pid, int32_t *status);
 int		set(t_env	*env, char *key, char *value);
@@ -132,7 +136,7 @@ int		syntax_error(t_token *token);
 t_token	*ft_list(t_token *token, t_env *env);
 t_token	*expanding(t_token *token, t_env *env);
 // int here_doc(t_token *token);
-char 	*expand(char *s, t_env *envp);
+char	*expand(char *s, t_env *envp);
 int		pipe_error(t_token *s);
 int		red_output_error(t_token *s);
 int		red_input_error(t_token *s);
@@ -162,7 +166,7 @@ void	signals_for_child(void);
 void	sig_handler_for_process(int signum);
 void	signal_for_heredoc(void);
 void	sig_quit(int signum);
-void	ignore_exit_signal();
+void	ignore_exit_signal(void);
 int		ft_len(char const *s, char c);
 int		pwd(void);
 int		cd(char **arr, t_env *env);
@@ -175,8 +179,13 @@ int		execution(t_token *lst, t_env *env);
 void	builtins(t_token *lst, t_env *env);
 int		single_builtins(t_token *lst, t_env *env);
 char	*ft_env(t_env *env, char *s);
+void	sort_env(t_env *env);
 int		exiting(t_token *lst);
-
+bool	check_valid_identifier(char *s);
+int		echo(t_token *lst);
+int		export(char **arr, t_env *env);
+int		unset(t_token *lst, t_env *env);
+char	**env_to_arr(t_env *env);
 
 //env
 t_env	*envir(char **envp);
