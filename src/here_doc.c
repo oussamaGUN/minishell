eaae0019fa3fca6e3b24a268731a62e73b18fcc6
BL4KCY <mostafafarssiui@gmail.com>
2024-05-03 22:48:08 +0100

    Fix memory leak in here_doc.c

--- src/here_doc.c
+++ src/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: melfersi <melfersi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 06:55:58 by melfersi          #+#    #+#             */
-/*   Updated: 2024/05/02 07:10:48 by melfersi         ###   ########.fr       */
+/*   Updated: 2024/05/03 16:11:08 by melfersi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,6 +83,7 @@ t_token	*child_process_for_heredoc(t_token *token, t_env *env, int file)
 		ft_putendl_fd(new, file);
 	}
 	close(token->fd[1]);
+	free(new);
 	exit(0);
 }
 
